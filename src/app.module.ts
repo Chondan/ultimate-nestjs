1ce0d697c3cdb1e64fe7eda7989b08c2a148bd72
Chondan Susuwan <chondansusuwan@outlook.com>
2026-01-06 10:57:47 +0700

    Setup typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { ApiModule } from './api/api.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import appConfig from './config/app.config';
 import databaseConfig from './config/database.config';
 import { Environment } from './constants/app.constant';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GlobalConfig } from './config/config.type';
 
 @Module({
-    imports: [ApiModule],
+    imports: [],
     controllers: [],
     providers: [],
 })
@@ -15,6 +17,7 @@ export class AppModule {
         return {
             module: AppModule,
             imports: [
+                ApiModule,
                 ConfigModule.forRoot({
                     isGlobal: true,
                     load: [appConfig, databaseConfig],
@@ -24,6 +27,26 @@ export class AppModule {
                      * rely strictly on OS-level environment variables (ideal for Production/Docker).
                      */
                     ignoreEnvFile: env === Environment.Production,
+                }),
+                TypeOrmModule.forRootAsync({
+                    imports: [ConfigModule],
+                    inject: [ConfigService],
+                    useFactory: (configService: ConfigService<GlobalConfig>) => ({
+                        type: 'postgres',
+                        host: configService.get('database.DATABASE_HOST', { infer: true }),
+                        port: configService.get('database.DATABASE_PORT', { infer: true }),
+                        username: configService.get('database.DATABASE_USERNAME', { infer: true }),
+                        password: configService.get('database.DATABASE_PASSWORD', { infer: true }),
+                        database: configService.get('database.DATABASE_NAME', { infer: true }),
+
+                        // Best Practice: Load entities automatically
+                        autoLoadEntities: true,
+                        // ⚠️ Set to false in production to prevent data loss
+                        synchronize: false,
+                        // Recommended for PostgreSQL performance
+                        logging: env === Environment.Development,
+                        ssl: configService.get('database.DATABASE_SSL', { infer: true }) ? { rejectUnauthorized: false } : false,
+                    }),
                 }),
             ],
         };
--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -10,16 +10,22 @@ export class DatabaseConfig {
     @IsNotEmpty()
     DATABASE_PORT: number;
     @IsNotEmpty()
+    DATABASE_NAME: string;
+    @IsNotEmpty()
     DATABASE_USERNAME: string;
     @IsNotEmpty()
     DATABASE_PASSWORD: string;
+    @IsNotEmpty()
+    DATABASE_SSL: boolean;
 }
 
 export default createConfigLoader(CONFIG_PREFIX, DatabaseConfig, () => {
     return {
         DATABASE_HOST: process.env.DATABASE_HOST || '',
         DATABASE_PORT: parseInt(process.env.DATABASE_PORT || '', 10),
+        DATABASE_NAME: process.env.DATABASE_NAME || '',
         DATABASE_USERNAME: process.env.DATABASE_USERNAME || '',
         DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || '',
+        DATABASE_SSL: process.env.DATABASE_SSL === 'true',
     };
 });
